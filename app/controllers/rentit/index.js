8c38b963d48a9e6e38fa0d5b2d44940219eb8bf2
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-02 09:43:37 -0500

    Move category listings off outermost route.

--- app/controllers/rentit/index.js
+++ app/controllers/rentit/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  allCategories: function() {
+    var allCats = [];
+    this.get('model').forEach(function(item) {
+      allCats.push(item.categories);
+    });
+    allCats = [].concat.apply([], allCats);
+    allCats = allCats.reduce(function(acc, item) {
+      return acc.indexOf(item) > -1 ? acc : acc + ',' + item;
+    }).split(',');
+    console.log(allCats);
+    return allCats;
+  }.property('model')
 });
--- app/routes/rentit/index.js
+++ app/routes/rentit/index.js
@@ -3,5 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return Ember.$.ajax("https://api.parse.com/1/classes/RentIt").then(function(res) { return res.results; });
-  }
+  },
+  
 });
